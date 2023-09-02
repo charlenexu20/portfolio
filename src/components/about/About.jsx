3082106a1ed5b81eda2590fde56280b1fd5d73d6
charlene <charlene.xu92@gmail.com>
2023-09-01 22:40:08 -0700

    Complete about component styling and layout

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 import "./about.css";
+import ME from "../../assets/me.jpg";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="about-me" />
+          </div>
+        </div>
+        <div className="about__content">
+          <h2 className="about__content-title">About Me</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <a href="#contact" className="btn">
+            Let&apos;s Chat
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;

1ffc1b65072b71f232058ee765ff803c08fe5c02
charlene <charlene.xu92@gmail.com>
2023-09-08 22:13:24 -0700

    Complete footer component styling and layout

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./footer.css";
+import { BsSuitHeartFill } from "react-icons/bs";
+import HeaderSocial from "../header/HeaderSocial";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  let getYear = () => {
+    let currentYear = new Date().getFullYear();
+    return currentYear;
+  };
+
+  return (
+    <footer>
+      <HeaderSocial customClass="footer__social" />
+      <div className="footer__copyright">
+        <small>
+          &copy; CX {getYear()}. Made with{" "}
+          <BsSuitHeartFill className="footer__copyright-icon" /> by Charlene Xu
+        </small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

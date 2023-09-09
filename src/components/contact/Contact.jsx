de36338d5e9590fa8a1c06f01781e21745c39d86
charlene <charlene.xu92@gmail.com>
2023-09-08 18:26:35 -0700

    Complete contact component styling and layout

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,79 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./contact.css";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  const form = useRef();
+
+  const notify = () => {
+    toast.success("Thanks for reaching out! I'll get back to you soon.", {
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_l185l78",
+        "template_8jp99lp",
+        form.current,
+        process.env.REACT_APP_API_KEY,
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          notify();
+        },
+        (error) => {
+          console.log(error.text);
+        },
+      );
+    e.target.reset();
+  };
+
+  return (
+    <section id="contact" className="contact">
+      <h2 className="contact__title">Get In Touch</h2>
+      <div className="container contact__container">
+        <form className="contact-form" ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Name" required />
+          <input type="email" name="email" placeholder="Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            SEND MESSAGE
+          </button>
+        </form>
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
